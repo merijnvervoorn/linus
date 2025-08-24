3a5bb3e8d07c851295396e16e0655878e948cfbd
Merijn <161606054+merijnvervoorn@users.noreply.github.com>
2025-08-24 13:57:23 +0200

    Adding winapps and some other changes

--- linus.sh
+++ linus.sh
@@ -48,13 +48,13 @@ install_essentials() {
     echo -e "${YELLOW}Installing essentials...${RESET}"
     
     # Installing command-line utilities
-    sudo apt install curl wget neofetch samba
+    sudo apt install curl wget neofetch samba ssh
     
     # Update
     sudo apt update && sudo apt upgrade -y
     
     # Apt
-    sudo apt install -y audacity calibre firefox flatpak flathub gimp git gnome-tweaks gnome-shell-extensions kdenlive libreoffice onedrive python3 python3-pip vlc 
+    sudo apt install -y audacity calibre flatpak flathub gimp git gnome-tweaks gnome-shell-extensions kdenlive libreoffice onedrive python3 python3-pip vlc 
     
     # Removing the office applications I don't need
     sudo rm /usr/share/applications/libreoffice-draw.desktop
@@ -82,7 +82,7 @@ install_extras() {
     echo -e "${YELLOW}Installing extras...${RESET}"
     
     # Apt
-    sudo apt install -y steam virtualbox
+    sudo apt install -y steam virtualbox 
     
     #Tailscale and EduVPN
     curl -fsSL https://tailscale.com/install.sh | sh
@@ -127,8 +127,35 @@ install_extras() {
     sudo apt-get install -f
     
     rm lutris.deb
+    
+    # Docker
+    sudo apt-get update
+	sudo apt-get install ca-certificates curl
+	sudo install -m 0755 -d /etc/apt/keyrings
+	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+	# Add the repository to Apt sources:
+	echo \
+	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+	  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+	  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	sudo apt-get update
+	
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	
+	wget -O docker-desktop-amd64.deb https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
+	sudo apt install ~/Downloads/docker-desktop-amd64.deb
         
     echo -e "${GREEN}Done installing extras!${RESET}"
+    log_end
+}
+
+firefox() {
+    log_start
+    echo -e "${YELLOW}Installing Firefox...${RESET}"
+    sudo apt install firefox
+    echo -e "${GREEN}Firefox installed!${RESET}"
     log_end
 }
 
@@ -230,6 +257,205 @@ dotfiles() {
     log_end
 }
 
+
+
+winapps() {
+	mkdir ~/.config/winapps/
+	cd ~/.config/winapps
+	wget https://github.com/winapps-org/winapps/blob/main/compose.yaml
+	# Print the message
+	echo "Now you should change the username and password"
+
+	# Wait for the user to press any key
+	read -n 1 -s -r -p "Press any key to open the file..."
+	
+	nano compose.yaml
+	# Check if the user saved the file
+	if [ $? -eq 0 ]; then
+		echo "File saved successfully. Continuing with the script..."
+		# Add further commands here
+		
+		docker compose --file ./compose.yaml up
+		
+		    if ask "Is the windows machine set up? (Have you installed the applications you need?)"; then
+			   # Add commands
+			   sudo apt install -y curl dialog freerdp3-x11 git iproute2 libnotify-bin netcat-openbsd
+			   touch ~/.config/winapps/winapps.conf
+			   cat << EOF >> winapps.conf
+			   ##################################
+				#   WINAPPS CONFIGURATION FILE   #
+				##################################
+
+				# INSTRUCTIONS
+				# - Leading and trailing whitespace are ignored.
+				# - Empty lines are ignored.
+				# - Lines starting with '#' are ignored.
+				# - All characters following a '#' are ignored.
+
+				# [WINDOWS USERNAME]
+				RDP_USER="MyWindowsUser"
+
+				# [WINDOWS PASSWORD]
+				# NOTES:
+				# - If using FreeRDP v3.9.0 or greater, you *have* to set a password
+				RDP_PASS="MyWindowsPassword"
+
+				# [WINDOWS DOMAIN]
+				# DEFAULT VALUE: '' (BLANK)
+				RDP_DOMAIN=""
+
+				# [WINDOWS IPV4 ADDRESS]
+				# NOTES:
+				# - If using 'libvirt', 'RDP_IP' will be determined by WinApps at runtime if left unspecified.
+				# DEFAULT VALUE:
+				# - 'docker': '127.0.0.1'
+				# - 'podman': '127.0.0.1'
+				# - 'libvirt': '' (BLANK)
+				RDP_IP="127.0.0.1"
+
+				# [VM NAME]
+				# NOTES:
+				# - Only applicable when using 'libvirt'
+				# - The libvirt VM name must match so that WinApps can determine VM IP, start the VM, etc.
+				# DEFAULT VALUE: 'RDPWindows'
+				VM_NAME="RDPWindows"
+
+				# [WINAPPS BACKEND]
+				# DEFAULT VALUE: 'docker'
+				# VALID VALUES:
+				# - 'docker'
+				# - 'podman'
+				# - 'libvirt'
+				# - 'manual'
+				WAFLAVOR="docker"
+
+				# [DISPLAY SCALING FACTOR]
+				# NOTES:
+				# - If an unsupported value is specified, a warning will be displayed.
+				# - If an unsupported value is specified, WinApps will use the closest supported value.
+				# DEFAULT VALUE: '100'
+				# VALID VALUES:
+				# - '100'
+				# - '140'
+				# - '180'
+				RDP_SCALE="100"
+
+				# [MOUNTING REMOVABLE PATHS FOR FILES]
+				# NOTES:
+				# - By default, `udisks` (which you most likely have installed) uses /run/media for mounting removable devices.
+				#   This improves compatibility with most desktop environments (DEs).
+				# ATTENTION: The Filesystem Hierarchy Standard (FHS) recommends /media instead. Verify your system's configuration.
+				# - To manually mount devices, you may optionally use /mnt.
+				# REFERENCE: https://wiki.archlinux.org/title/Udisks#Mount_to_/media
+				REMOVABLE_MEDIA="/run/media"
+
+				# [ADDITIONAL FREERDP FLAGS & ARGUMENTS]
+				# NOTES:
+				# - You can try adding /network:lan to these flags in order to increase performance, however, some users have faced issues with this.
+				# DEFAULT VALUE: '/cert:tofu /sound /microphone +home-drive'
+				# VALID VALUES: See https://github.com/awakecoding/FreeRDP-Manuals/blob/master/User/FreeRDP-User-Manual.markdown
+				RDP_FLAGS="/cert:tofu /sound /microphone +home-drive"
+
+				# [DEBUG WINAPPS]
+				# NOTES:
+				# - Creates and appends to ~/.local/share/winapps/winapps.log when running WinApps.
+				# DEFAULT VALUE: 'true'
+				# VALID VALUES:
+				# - 'true'
+				# - 'false'
+				DEBUG="true"
+
+				# [AUTOMATICALLY PAUSE WINDOWS]
+				# NOTES:
+				# - This is currently INCOMPATIBLE with 'docker' and 'manual'.
+				# - See https://github.com/dockur/windows/issues/674
+				# DEFAULT VALUE: 'off'
+				# VALID VALUES:
+				# - 'on'
+				# - 'off'
+				AUTOPAUSE="off"
+
+				# [AUTOMATICALLY PAUSE WINDOWS TIMEOUT]
+				# NOTES:
+				# - This setting determines the duration of inactivity to tolerate before Windows is automatically paused.
+				# - This setting is ignored if 'AUTOPAUSE' is set to 'off'.
+				# - The value must be specified in seconds (to the nearest 10 seconds e.g., '30', '40', '50', etc.).
+				# - For RemoteApp RDP sessions, there is a mandatory 20-second delay, so the minimum value that can be specified here is '20'.
+				# - Source: https://techcommunity.microsoft.com/t5/security-compliance-and-identity/terminal-services-remoteapp-8482-session-termination-logic/ba-p/246566
+				# DEFAULT VALUE: '300'
+				# VALID VALUES: >=20
+				AUTOPAUSE_TIME="300"
+
+				# [FREERDP COMMAND]
+				# NOTES:
+				# - WinApps will attempt to automatically detect the correct command to use for your system.
+				# DEFAULT VALUE: '' (BLANK)
+				# VALID VALUES: The command required to run FreeRDPv3 on your system (e.g., 'xfreerdp', 'xfreerdp3', etc.).
+				FREERDP_COMMAND=""
+
+				# [TIMEOUTS]
+				# NOTES:
+				# - These settings control various timeout durations within the WinApps setup.
+				# - Increasing the timeouts is only necessary if the corresponding errors occur.
+				# - Ensure you have followed all the Troubleshooting Tips in the error message first.
+
+				# PORT CHECK
+				# - The maximum time (in seconds) to wait when checking if the RDP port on Windows is open.
+				# - Corresponding error: "NETWORK CONFIGURATION ERROR" (exit status 13).
+				# DEFAULT VALUE: '5'
+				PORT_TIMEOUT="5"
+
+				# RDP CONNECTION TEST
+				# - The maximum time (in seconds) to wait when testing the initial RDP connection to Windows.
+				# - Corresponding error: "REMOTE DESKTOP PROTOCOL FAILURE" (exit status 14).
+				# DEFAULT VALUE: '30'
+				RDP_TIMEOUT="30"
+
+				# APPLICATION SCAN
+				# - The maximum time (in seconds) to wait for the script that scans for installed applications on Windows to complete.
+				# - Corresponding error: "APPLICATION QUERY FAILURE" (exit status 15).
+				# DEFAULT VALUE: '60'
+				APP_SCAN_TIMEOUT="60"
+
+				# WINDOWS BOOT
+				# - The maximum time (in seconds) to wait for the Windows VM to boot if it is not running, before attempting to launch an application.
+				# DEFAULT VALUE: '120'
+				BOOT_TIMEOUT="120"
+				EOF
+				
+				# Print the message
+				echo "Now you should change the username and password"
+
+				# Wait for the user to press any key
+				read -n 1 -s -r -p "Press any key to open the file..."
+				
+			   nano winapps.conf
+			   if [ $? -eq 0 ]; then
+					echo "File saved successfully. Continuing with the script..."
+					# Add further commands here
+					
+					wget ttps://raw.githubusercontent.com/winapps-org/winapps/main/setup.sh
+					sudo mkdir /root/.config/winapps
+					sudo ln -s ~/.config/winapps/winapps.conf /root/.config/winapps/winapps.conf
+					sudo bash ~/.config/winapps/setup.sh
+					
+				else
+					echo "File was not saved. Exiting."
+					exit 1
+				fi
+			   
+			fi
+			echo
+		
+		
+	else
+		echo "File was not saved. Exiting."
+		exit 1
+	fi
+}
+
+
+
 ask() {
     local prompt="$1"
     local response
@@ -274,6 +500,10 @@ main() {
             install_extras
         fi
         echo
+        if ask "How about Firefox?"; then
+            firefox
+        fi
+        echo
         if ask "How about a third browser (Brave)?"; then
             brave
         fi
@@ -297,10 +527,18 @@ main() {
     fi
     echo
     
-    if ask "Do you want to set up your dotfiles? (Make sure to get a Personal Access Token from Github for logging in)"; then
+    echo -e "Also get a Personal Access Token: https://github.com/settings/tokens \n"
+    
+    if ask "Do you want to set up your dotfiles?"; then
         dotfiles
     fi
     echo    
+
+    
+    if ask "Do you want to install WinApps (for using Windows-only apps)?"; then
+       winapps
+    fi
+    echo
     
     
     #if ask "question"; then
